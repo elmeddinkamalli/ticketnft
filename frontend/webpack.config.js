46405915d69253ba5082a1d01fe802dba85f0ccf
elmeddinkamalli <elmeddinkamalli@gmail.com>
2022-10-21 19:56:58 +0400

    first commit

---
+++ frontend/webpack.config.js
@@ -0,0 +1,89 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const dotenv = require("dotenv");
+
+const imageLoaderConfiguration = {
+  test: /\.(svg)$/,
+  exclude: /node_modules/,
+  use: {
+    loader: "file-loader",
+    options: {
+      name: "[name].[ext]",
+    },
+  },
+};
+
+dotenv.config();
+
+module.exports = {
+  mode: "development",
+  entry: path.resolve(__dirname, "./src/index.js"),
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "main.bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.(sass|css)$/i,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+      {
+        test: /\.svg$/i,
+        issuer: /\.[jt]sx?$/,
+        use: ["@svgr/webpack"],
+      },
+      imageLoaderConfiguration,
+      {
+        test: /\.(png|jpg)$/,
+        loader: "url-loader",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+    alias: {
+      Utilities: path.resolve(__dirname, "src/utils/"),
+      images: path.resolve(__dirname, "src/assets/static"),
+    },
+  },
+  devtool: "eval-cheap-module-source-map",
+  plugins: [
+    new CleanWebpackPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      title: "Empty project",
+      template: path.resolve(__dirname, "./src/index.html"),
+    }),
+    new MiniCssExtractPlugin(),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "./src/assets/static", to: "static" }],
+    }),
+    new webpack.DefinePlugin({
+      "process.env": JSON.stringify(process.env),
+    }),
+  ],
+  devServer: {
+    static: path.join(__dirname, "dist"),
+    compress: true,
+    port: 9000,
+    open: true,
+    hot: true,
+    historyApiFallback: true,
+  },
+};
